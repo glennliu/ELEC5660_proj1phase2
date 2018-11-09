ed3acb7054f3412a8736b8fa4835c6d95bb1211a
Chuhao Liu <chuhao.liu@gmail.com>
2018-11-09 14:19:56 +0800

    generated 1D trajectory

--- code/test_trajectory.m
+++ code/test_trajectory.m
@@ -30,22 +30,21 @@ path2 = [0.5 0.5 1.0 ; ...
 
     
 
-h1 = subplot(3,3,1);
-h2 = subplot(3,3,2);
-h3 = subplot(3,3,3);
-h4 = subplot(3,3,4);
-h5 = subplot(3,3,5);
-h6 = subplot(3,3,6);
-h7 = subplot(3,3,7);
-h8 = subplot(3,3,8);
-h9 = subplot(3,3,9);
-set(gcf, 'Renderer', 'painters');
-set(gcf, 'Position', [100, 100, 1400, 1000]);
+% h1 = subplot(3,3,1);
+% h2 = subplot(3,3,2);
+% h3 = subplot(3,3,3);
+% h4 = subplot(3,3,4);
+% h5 = subplot(3,3,5);
+% h6 = subplot(3,3,6);
+% h7 = subplot(3,3,7);
+% h8 = subplot(3,3,8);
+% h9 = subplot(3,3,9);
+% set(gcf, 'Renderer', 'painters');
+% set(gcf, 'Position', [100, 100, 1400, 1000]);
 % set(gcf, 'WindowStyle','Modal');
 
-
 % Trajectory Generator
-trajectory_generator([], path1, h1);
+trajectory_generator([], path1);
 
 % Run Trajectory
-run_trajectory_readonly(h1, h2, h3, h4, h5, h6, h7, h8, h9);
+% run_trajectory_readonly(h1, h2, h3, h4, h5, h6, h7, h8, h9);
